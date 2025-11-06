ed2a9ad9097d5b462932270e21405a4ccb57e465
gymynnym <gymynnym@gmail.com>
2025-11-06 22:08:20 +0900

    feat: Add auto-reload when configuration changes
    - Add listener for `vscode-gitui` configuration changes to trigger reload
    - Add `workspaceContains:.git` to `activationEvents`

--- src/extension.ts
+++ src/extension.ts
@@ -21,6 +21,11 @@ export function activate(context: vscode.ExtensionContext) {
   const disposables = [
     vscode.commands.registerCommand('vscode-gitui.open', () => handleGitClientCommand(openGitClient)),
     vscode.commands.registerCommand('vscode-gitui.reload', () => handleGitClientCommand(reloadGitClient)),
+    vscode.workspace.onDidChangeConfiguration((event) => {
+      if (event.affectsConfiguration('vscode-gitui')) {
+        reloadGitClient();
+      }
+    }),
   ];
   disposables.forEach((disposable) => context.subscriptions.push(disposable));
 }
